bcb0ee7e059d09ad4cabcd763cb07be7744693db
shincling <shijing609@163.com>
2015-11-18 15:37:09 +0800

    增加了story_id和dict_unknown参数，可以还原出被放到unknown的词。

--- MemN2N-babi-matlab/real_word.m
+++ MemN2N-babi-matlab/real_word.m
@@ -1,8 +1,10 @@
-function word = real_word( index,dict )
+function word = real_word( index,dict,story_id,dict_of_unknown )
 %REAL_WORD Summary of this function goes here
 %   Detailed explanation goes here
       if index==1
           word='';
+      elseif  index>=dict('Unknown1')
+          word=dict_of_unknown{index-dict('Unknown1')+1,story_id};
       else
           for iiii=dict.keys()
            try
