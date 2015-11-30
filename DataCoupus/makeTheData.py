29e98d15d26c17bb873667c61254c117f3175f2f
shincling <shijing609@163.com>
2015-11-30 23:29:34 +0800

    add the namelist_question

---
+++ DataCoupus/makeTheData.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+__author__ = 'shin'
+import re
+from namelist_question import namelist_question
+#from namelist_answer import namelist_answer
+
+print 'name question:%d'%len(namelist_question)
+
+print namelist_question[0]
+
+
