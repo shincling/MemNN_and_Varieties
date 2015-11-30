7bb8469d57ca58ceeb0ac5755a9597b47cfc27b3
shincling <shijing609@163.com>
2015-12-01 00:08:42 +0800

    完成name_List_answer切分好的模板，利用了周杰伦名字做一个小的trick保证切分正确。

--- DataCoupus/makeTheData.py
+++ DataCoupus/makeTheData.py
@@ -1,11 +1,21 @@
 # -*- coding: utf8 -*-
 __author__ = 'shin'
 import re
+import jieba
 from namelist_question import namelist_question
-#from namelist_answer import namelist_answer
+from namelist_answer import namelist_answer
 
 print 'name question:%d'%len(namelist_question)
+print 'name answer:%d\n'%len(namelist_answer)
 
-print namelist_question[0]
+storyNumber=1000
+fw=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/ticket_shin.txt','w')
+
+for story_ind in range(storyNumber):
+    fw.write('1 您好 ， 机票 预订 中心 ， 需要 我 为 你 做些 什么 ？\n')
+    fw.write('2 我 想 预订 机票 。\n')
+
+
+
 
 
