5caf667da573831433bd577ee9d60faea1175401
shincling <shijing609@163.com>
2015-12-03 15:05:43 +0800

    destinationlist_answer 增加版

--- DataCoupus/destinationlist_answer.py
+++ DataCoupus/destinationlist_answer.py
@@ -44,6 +44,19 @@ destinationlist_answer.append('我要到北京去。')
 destinationlist_answer.append('飞往北京的飞机。')
 destinationlist_answer.append('买去北京的机票。')
 destinationlist_answer.append('我要订去北京的飞机。')
+
+destinationlist_answer.append('北京')
+destinationlist_answer.append('我要去北京')
+destinationlist_answer.append('到北京')
+destinationlist_answer.append('我到北京')
+destinationlist_answer.append('我去北京')
+destinationlist_answer.append('到北京的机票')
+destinationlist_answer.append('飞北京的机票')
+destinationlist_answer.append('到北京的票')
+destinationlist_answer.append('飞北京的票')
+destinationlist_answer.append('去北京的机票')
+destinationlist_answer.append('去北京的票')
+
 destinationlist_answer_cut=[]
 
 for ans in destinationlist_answer:
