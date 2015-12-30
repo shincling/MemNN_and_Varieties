195815e5158e4eacd84032036d9479b261a24b7f
shincling <shijing609@163.com>
2015-12-30 14:38:41 +0800

    log

--- DataCoupus/makeTheData_Conversation_withoutAns.py
+++ DataCoupus/makeTheData_Conversation_withoutAns.py
@@ -38,7 +38,7 @@ print 'phone question:%d'%len(phonelist_question_cut)
 print 'phone answer:%d\n'%len(phonelist_answer_cut)
 
 storyNumber=1000
-fw=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/ticket_randOrder_ANS_slot_test.txt','w')
+fw=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/ticket_randOrder_ANS_slot_train.txt','w')
 
 familyName=['号','王','李','赵','周','吴','顾','郑','何','万','黄','周','吴','徐','孙','胡','朱','高',
            '林','何','郭','马','罗','梁','宋','谢','韩','唐','冯','于','董','萧','程','曹','袁','邓',
