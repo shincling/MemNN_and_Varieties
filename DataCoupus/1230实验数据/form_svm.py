0a6a0e4ee67bac3544ae47170899fa8d19f9feea
shincling <shijing609@163.com>
2015-12-31 17:27:42 +0800

    从mat的矩阵数据中，把status 和next对应的五十维向量跑出来。

--- DataCoupus/1230实验数据/form_svm.py
+++ DataCoupus/1230实验数据/form_svm.py
@@ -3,12 +3,37 @@ __author__ = 'shin'
 
 import matlab.engine
 
-Matlabpath='/home/shin/DeepLearning/MemoryNetwork/MemNN/MemN2N-babi-matlab'
+Matlabpath='/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/out_presentation'
 
 eng = matlab.engine.start_matlab()
 eng.eval('cd %s;' % (Matlabpath), nargout=0)
 
-for i in range(1,1001):
+f_status=open('svm_train_status.txt','w')
+f_next=open('svm_train_next.txt','w')
+
+for i in range(1,2):
+    result=eng.eval("load('%d.mat')"%i)['output']
+
+    for step_batch in range(32):
+        '''
+        for j in range(50):
+            one_example=result[j][step_batch]
+            if step_batch%2==0:
+                f_status.write('%d:%f '%(j+1,result[j][(step_batch)/2]))
+            if step_batch%2==1:
+                f_next.write('%d:%f '%(j+1,result[j][(step_batch+1)/2]))
+        '''
+        if step_batch%2==0:
+            for j in range(50):
+                f_status.write('%d:%f '%(j+1,result[j][(step_batch)]))
+            f_status.write('\n')
+        if step_batch%2==1:
+            for j in range(50):
+                f_next.write('%d:%f '%(j+1,result[j][step_batch]))
+            f_next.write('\n')
+
+
+
 
 
 
