e3cbdaf23f01ce3b70f8df827e6891ede5a710a1
shincling <shijing609@163.com>
2016-01-12 14:50:47 +0800

    笔记本上的最后一个版本，之后跟git上的合并。

--- DataCoupus/1230实验数据/calculate_the_error.py
+++ DataCoupus/1230实验数据/calculate_the_error.py
@@ -244,12 +244,13 @@ totallist.append('已经为您预订完毕。')
 
 # f=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa28','r')
 # f=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa29_0104/qa29','r')
-f=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa31_0105/qa31_noSlot','r')
+f=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa31_0105/qa31_oneSlot','r')
 out=f.read()
 out=out.split('\r\n')#[:-1]
 # ff=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa28_ticket_randOrder_ANS_slot_test.txt','r')
 # ff=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa29_ticket_randOrder_withSlot_test.txt','r')
-ff=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa31_noSlot_ticket_rand_withSlot_test.txt','r')
+# ff=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa31_noSlot_ticket_rand_withSlot_test.txt','r')
+ff=open('/home/shin/DeepLearning/MemoryNetwork/MemNN/DataCoupus/1230实验数据/qa31_ticket_randOrderAnsSent_withSlot_test.txt','r')
 
 storys=ff.read().split('谢谢 。\n1 ')
 one_story_list=[]
