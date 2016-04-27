04fcf40832ac1624791cb9c481b501f6c18e8c09
shincling <shijing609@163.com>
2016-04-27 10:41:54 +0800

    开始尝试Unknown_networks

--- MemN2N_python/Unknown_networks/unknown_main.py
+++ MemN2N_python/Unknown_networks/unknown_main.py
@@ -11,6 +11,61 @@ from sklearn import metrics
 from sklearn.preprocessing import LabelBinarizer,label_binarize
 
 
+class Model:
+    def __init__(self, train_file, test_file, batch_size=32, embedding_size=20, max_norm=40, lr=0.01, num_hops=3, adj_weight_tying=True, linear_start=True, enable_time=False,**kwargs):
+        train_lines, test_lines = self.get_lines(train_file), self.get_lines(test_file)
+        lines = np.concatenate([train_lines, test_lines], axis=0) #直接头尾拼接
+        vocab, word_to_idx, idx_to_word, max_seqlen, max_sentlen = self.get_vocab(lines)
+        #C是document的列表，Q是定位问题序列的列表，Y是答案组成的列表，目前为知都是字符形式的，没有向量化#
+        self.data = {'train': {}, 'test': {}}  #各是一个字典
+        S_train, self.data['train']['C'], self.data['train']['Q'], self.data['train']['Y'] = self.process_dataset(train_lines, word_to_idx, max_sentlen, offset=0)
+        S_test, self.data['test']['C'], self.data['test']['Q'], self.data['test']['Y'] = self.process_dataset(test_lines, word_to_idx, max_sentlen, offset=len(S_train))
+        S = np.concatenate([np.zeros((1, max_sentlen), dtype=np.int32), S_train, S_test], axis=0)
+        for i in range(min(10,len(self.data['test']['C']))):
+            for k in ['C', 'Q', 'Y']:
+                print k, self.data['test'][k][i]
+        print 'batch_size:', batch_size, 'max_seqlen:', max_seqlen, 'max_sentlen:', max_sentlen
+        print 'sentences:', S.shape
+        print 'vocab size:', len(vocab)
+
+        for d in ['train', 'test']:
+            print d,
+            for k in ['C', 'Q', 'Y']:
+                print k, self.data[d][k].shape,
+            print ''
+
+        vocab=[]
+        for i in range(len(idx_to_word)):
+            vocab.append(idx_to_word[i+1])
+
+
+        lb = LabelBinarizer()
+        # lb.fit(list(vocab))
+        # vocab = lb.classes_.tolist()
+
+
+        self.enable_time=enable_time
+        self.batch_size = batch_size
+        self.max_seqlen = max_seqlen
+        self.max_sentlen = max_sentlen if not enable_time else max_sentlen+1
+        self.embedding_size = embedding_size
+        self.num_classes = len(vocab) + 1
+        self.vocab = vocab
+        self.adj_weight_tying = adj_weight_tying
+        self.num_hops = num_hops
+        self.lb = lb
+        self.init_lr = lr
+        self.lr = self.init_lr
+        self.max_norm = max_norm
+        self.S = S
+        self.idx_to_word = idx_to_word
+        self.nonlinearity = None if linear_start else lasagne.nonlinearities.softmax
+        self.word_to_idx=word_to_idx
+
+        self.build_network(self.nonlinearity)
+
+def str2bool(v):
+    return v.lower() in ('yes', 'true', 't', '1')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -18,6 +73,7 @@ def main():
     parser.add_argument('--task', type=int, default=35, help='Task#')
     parser.add_argument('--train_file', type=str, default='', help='Train file')
     parser.add_argument('--test_file', type=str, default='', help='Test file')
+    parser.add_argument('--back_method', type=str, default='sgd', help='Train Method to bp')
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
     parser.add_argument('--embedding_size', type=int, default=100, help='Embedding size')
     parser.add_argument('--max_norm', type=float, default=40.0, help='Max norm')
