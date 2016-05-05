55bfe6d64d2e44eb9fa09698afa207a6f467a68e
shincling <shijing609@163.com>
2016-05-05 17:10:55 +0800

    the strcture with bugs

---
+++ DataCoupus/201604/unk_sent_product.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+import random
+def main():
+    f=open(output_path,'w')
+    for i in range(sent_num):
+        num=i
+        slot_num=random.randint(1,10)
+        name=chr(random.randint(97, 122))+chr(random.randint(97, 122))+chr(random.randint(97, 122))+chr(random.randint(97, 122))+chr(random.randint(97, 122))
+        phone=str(random.randint(10000000,99999999))+' 。'
+        question=random.choice(['phone','name'])
+        ans=slot_num+3 if question=='name' else slot_num+7
+        line=str(i)+'\t'
+        line+='哈 '*slot_num
+        line+='我的 名字 叫 {} ,'.format(name)
+        line+=' 电话 是 {} 。\t'.format(phone)
+        line+='{} ?\t{}'.format(question,ans)
+        f.writelines(line)
+
+
+if __name__=='__main__':
+    output_path='unk_sent_train.txt'
+    sent_num=1000
+    main()
